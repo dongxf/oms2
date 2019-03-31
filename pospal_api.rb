de6e057ca7a0f85b96ef65e3003fd97b81621bb1
dongxf <dongxf@foodtrust.cn>
2019-03-31 21:00:22 +0800

    add pospal_api

---
+++ pospal_api.rb
@@ -0,0 +1,45 @@
+﻿#encoding: utf-8
+#this file can be used in other ruby file by loading 'pospal_api.rb'
+
+require 'digest/md5'
+require 'net/http'
+require 'json'
+require 'date'
+require 'awesome_print'
+
+# request_body as Hash
+# return is Hash
+#
+# req = { 'appId'=> pospal_appid,     'orderNo'=> '19032703082989286104' }
+# res = post_to_posal :shipOder, req
+# puts res.body
+
+def post_to_pospal api_name, request_body 
+  api_urls={
+    :shipOrder => 'orderOpenApi/shipOrder',
+    :getCategoryPages => 'productOpenApi/queryProductCategoryPages'
+  }
+
+  #please set POSPAL_APPID and APPKEY in .bashrc
+  pospal_appid=ENV['POSPAL_APPID']
+  pospal_appkey=ENV['POSPAL_APPKEY']
+  base_url= 'https://area24-win.pospal.cn:443/pospal-api2/openapi/v1/'
+
+  
+  request_body.store('appId', pospal_appid)
+
+  uri = URI(base_url+api_urls[api_name])
+  res = Net::HTTP.start(uri.host, uri.port, use_ssl: true) do |http|
+    req = Net::HTTP::Post.new(uri)
+    req['User-Agent']= 'openApi'
+    req['Content-Type']= 'application/json; charset=utf-8'
+    req['accept-encoding']= 'gzip,deflate'
+    req['time-stamp']= Time.now.getutc
+    req['data-signature']= Digest::MD5.hexdigest(pospal_appkey + request_body.to_json)
+    req.body = request_body.to_json
+    http.request(req)
+  end
+
+  return JSON.parse(res.body) if !res.nil?
+
+end
