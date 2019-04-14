db52e57fecbb4be8d5d22d89c2adc9b4ffb9d70d
dongxf <dongxf@foodtrust.cn>
2019-04-14 20:21:34 +0800

    add client side pwoershell and wincron

---
+++ incoming_files_handler.ps1
@@ -0,0 +1,30 @@
+#This file is used in client side, not in pcron server, work with win cron task(incomingFilesHandler.xml)
+
+Set-Location d:\fc3.0\orders
+
+#print incoming order and move
+Get-ChildItem -Path d:\fc3.0\orders\incoming -Filter *-order-*.pdf -Recurse | ForEach-Object {
+    #Start-Process -FilePath $_.FullName -Verb Print #if using -Wait option, acrobat reader will stay there forever, else the file will be deleted before printed
+    Start-Process -FilePath $_.fullname -Verb Print -PassThru | %{sleep 10;$_} | kill 
+    Move-Item -Force -Path $_.FullName -Destination d:\fc3.0\orders\processed\orders
+}
+
+#print incoming line file and move
+Get-ChildItem -Path d:\fc3.0\orders\incoming -Filter *-line-*.pdf -Recurse | ForEach-Object {
+    #Start-Process -FilePath $_.FullName -Verb Print #if using -Wait option, acrobat reader will stay there forever, else the file will be deleted before printed
+    Start-Process -FilePath $_.fullname -Verb Print -PassThru | %{sleep 10;$_} | kill 
+    Move-Item -Force -Path $_.FullName -Destination d:\fc3.0\orders\processed\lines
+}
+
+#print incoming express label and move
+Get-ChildItem -Path d:\fc3.0\orders\incoming -Filter 三联单-*.pdf -Recurse | ForEach-Object {
+    #Start-Process -FilePath $_.FullName -Verb Print #if using -Wait option, acrobat reader will stay there forever, else the file will be deleted before printed
+    Start-Process -FilePath $_.fullname -Verb Print -PassThru | %{sleep 10;$_} | kill 
+    Move-Item -Force -Path $_.FullName -Destination d:\fc3.0\orders\processed\express-labels
+}
+
+
+#move incoming express data can be imported into express system, then print labels from express system
+Get-ChildItem -Path d:\fc3.0\orders\incoming -Filter *-CND-*.xls -Recurse | ForEach-Object {
+    Move-Item -Force -Path $_.FullName -Destination d:\fc3.0\orders\processed\express-data
+}
