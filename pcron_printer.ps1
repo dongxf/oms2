8530209aaf5248ed1819627541c14eb257b4199d
dongxf <dongxf@foodtrust.cn>
2019-04-14 16:47:34 +0800

    depreciate pcron_printer.ps1, because it has been merged into pcron_mailer.ps1

--- pcron_printer.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location d:\fc3.0\orders
-
-Get-ChildItem -Path d:\fc3.0\orders\incoming -Filter *.pdf -Recurse | ForEach-Object {
-    #Start-Process -FilePath $_.FullName -Verb Print #if using -Wait option, acrobat reader will stay there forever, else the file will be deleted before printed
-    Start-Process -FilePath $_.fullname -Verb Print -PassThru | %{sleep 10;$_} | kill 
-    Move-Item -Force -Path $_.FullName -Destination d:\fc3.0\orders\printed
-}
