7ef709cc4ddd29ddc9f762e785acb87e1c861f9c
Franklin Dong <dongxf@foodtrust.cn>
2019-05-30 18:57:03 +0800

    增加订阅设置数据导入工具sub_man.rb

--- sub_man.rb
+++ sub_man.rb
@@ -1,8 +1,16 @@
 #encoding: utf-8
 
-#this tool will synced wechat fans excel data goods exported data into wechat_fans db
-#user can drag exported file into auto_import and run this tool manually or automatically by win_cron
-
+#this tool will read subscription data exported from jinshuju and set status for in wechat_fans database
+#
+#完整流程，发送工具获取全部粉丝信息，导出EXCEL，另存为XLS
+#使用import_wechat_fans.rb将数据导入wechat_fans db
+#用金数据导出订阅设置，另存为XLS
+#使用sub_man将数据导入wechat_fans db
+#SELECT * FROM `wechat_fans` where subscrib_status is null or subscrib_status = 'yes'
+#注意在提供给发送工具之前，需要手工在数据库中将nick和avatar为空的数据重置
+#结果存为XLSX，用发送工具读入即可
+#
+#
 require 'mysql2'
 require 'awesome_print'
 require 'spreadsheet'
@@ -11,7 +19,7 @@ require 'spreadsheet'
 rds = Mysql2::Client.new(:host => ENV['RDS_AGENT'], :username => "psi_root", :port => '1401', :password => ENV['PSI_PASSWORD'], :encoding => 'utf8mb4' )
 wNickNames = {}
 overwrite_mode = false
-xls_file=".\\auto_import\\wechat_fans.xls"
+xls_file=".\\auto_import\\sub_data.xls"
 
 args = ''
 ARGV.each { |arg| args+=arg }
@@ -40,37 +48,32 @@ begin
     sheet1.each do |row|
         line_idx += 1
         next if line_idx == 1
-        openid = row[0]
-        escaped_nick_name = row[2].gsub("'","''") #用于SQL语句中的转义
+        openid = row[9]
+        escaped_nick_name = row[5].gsub("'","''") #用于SQL语句中的转义
         #will remove all links in description here
 
         if wNickNames[openid].nil?
             #如果openid不存在于数据库
-            puts "insert #{row[0]} #{row[2]}"
+            puts "insert #{escaped_nick_name} into wechat_fans db"
             sqlu = "insert into ogoods.wechat_fans( 
                         openid,avatar,nick_name,gender,location,subscription_time
                     ) values( 
-                        '#{row[0]}','#{row[1]}','#{escaped_nick_name}','#{row[3]}','#{row[4]}', '#{row[5]}'
+                        '#{row[9]}','#{row[10]}','#{escaped_nick_name}','','', ''
                     );"
             resu = rds.query(sqlu)
         else
-           #如果openid存在于数据库
-           if overwrite_mode || wNickNames[openid]!= row[2]
-                #如果昵称变化或者处于强制更新模式
-                puts "update #{row[2]} #{row[0]} #{wNickNames[openid]}"
+           #如果openid存在于数据库,根据设置更新允许推送消息的设置
+                puts "update subscrib_status for #{wNickNames[openid]}"
+                subscrib_status = row[1]=='不需要'? 'no':'yes'
                 sqlu = "update ogoods.wechat_fans set
-                    openid='#{row[0]}',avatar='#{row[1]}',nick_name='#{escaped_nick_name}',gender='#{row[3]}',location='#{row[4]}', 
-                    subscription_time='#{row[5]}'
-                    where openid = '#{row[0]}'
+                    subscrib_status='#{subscrib_status}'
+                    where openid = '#{openid}'
                 "
+                puts sqlu
                 resu = rds.query(sqlu)
-           end
         end
     end
-    #puts "total record in data file: #{line_idx}"
+    puts "total record in data file: #{line_idx}"
 rescue => e
     puts ">>>ERROR: #{e}"
 end
-
-
-#新的fans列表里不存在的情况未来可以进一步处理，例如标记为已取消关注
