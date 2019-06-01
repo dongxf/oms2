12b52a49da4b14e071c78fe8c497babb523e9fa1
Franklin Dong <dongxf@foodtrust.cn>
2019-06-01 18:13:41 +0800

    user_man.rb已经准备好，每天晚上11:30自动运行1次

--- user_man.rb
+++ user_man.rb
@@ -2,15 +2,12 @@
 
 # 1) This file read all users information from pospal and update the database
 # 2) Should be used before refund to make sure all CustomerUid is available
-#
+# 3) Only use 1 times in a day!
 
 require 'json'
 load 'user_api.rb'
 
 ##get_all_pospal_users will generate an json file under .\\auto_import\\, such as puser-20190601123011.json
-#pusers = get_all_pospal_users
-
 #pusers=JSON.parse IO.readlines(".\\auto_import\\pusers.json")[0]
-#update_userdb pusers
-
-p get_uid_by_number '136000600440'
+pusers = get_all_pospal_users
+update_userdb pusers
