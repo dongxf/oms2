c3484eddedd173f22344df083d9af5a9567c4e72
Franklin Dong <dongxf@foodtrust.cn>
2019-06-26 09:03:55 +0800

    rewrite wechat_api by using rest-client

--- award_man.rb
+++ award_man.rb
@@ -18,42 +18,54 @@ def award_point_to uid, point
     pospal_api :updateBiPi, req
 end
 
-connect = PG::connect(host:'rdsagent.foodtrust.cn',user:"postgres",password:'',dbname:"commento",port:"1403")
-results = connect.exec("select * from comments")
+def ms_test
+    rds = Mysql2::Client.new(:host => ENV['RDS_AGENT'], :username => "psi_root", :port => '1401', :password => ENV['PSI_PASSWORD'])
+    sql = 'select * from ogoods.wechat_fans'
+    res = rds.query(sql)
+    res.each { |r| p r['nick_name'] }
+end
 
-results.each{|result| p result["commenterhex"] }
-connect.finish
+def pg_test
+    connect = PG::connect(host: 'rdsagent.foodtrust.cn', user: "postgres", password: ENV['PSI_PASSWORD'], dbname: "commento", port: "1403" )
+    results = connect.exec("select * from comments")
+    results.each{|result| p result["commenterhex"] }
+    connect.finish
+end
 
 def send_points_notice openids
-    pointsNotice = {
-        'touser' =>  ' owHN1t0ETyOD1p_J324Gcb9twHuk',
-        'template_id' => 'RTPLTnnAvu-jI7fsAoWu0CwLpGZwoMBXK3bRlIxrkU8',
-        'url' => 'https://foodtrust.cn/',  
-        'data' => {
-            'first' => { 'value' => "这是测试，无论多少积分，你都要当做过眼云烟\n啊啊啊！(这是纯测试！)", 'color' => '#173177' },
-            'keyword1' => { 'value' =>"#{Time.now}", 'color' => '#173177' },
-            'keyword2' => { 'value' => '200分', 'color' => '#ff0000' },
-            'keyword3' => { 'value' => '撰写丰巢动态见证评论，点击详情可查看', 'color' => '#173177' },
-            'keyword4' => { 'value' => '1200分', 'color' => '#0000ff' },
-            'remark' => { 'value' =>"购买过该产品的用户有双倍积分\n没有购买过的用户加油哈哈哈\n\nFOODTRUST 丰巢有机\n每一天更安心的选择", 'color' => '#88b04b' },
+    notice = {
+        touser: 'owHN1t0ETyOD1p_J324Gcb9twHuk',
+        template_id:  'RTPLTnnAvu-jI7fsAoWu0CwLpGZwoMBXK3bRlIxrkU8',
+        url:  'https://foodtrust.cn/',  
+        data:  {
+            first:  { value:  "这是测试，无论多少积分，你都要当做过眼云烟\n啊啊啊！(这是纯测试！)", color:  '#173177' },
+            keyword1:  { value:   "#{Time.now}", color:  '#173177' },
+            keyword2:  { value:  '200分', color:  '#ff0000' },
+            keyword3:  { value:  '撰写丰巢动态见证评论，点击详情可查看', color:  '#173177' },
+            keyword4:  { value:  '1200分', color:  '#0000ff' },
+            remark:  { value: "购买过该产品的用户有双倍积分\n没有购买过的用户加油哈哈哈\n\nFOODTRUST 丰巢有机\n每一天更安心的选择", color:  '#88b04b' },
         }
     }
     wat = wechat_access_token
-    puts wat
     openids.each do |openid|
-        pointsNotice.store('touser',openid)
-        wechat_api :sendTemplateMessage, wat, pointsNotice
+        notice.store('touser',openid)
+        wechat_api :sendTemplateMessage, wat, notice
     end
 end
 
+#insert data into msqly: post_id, goods_code, good_name, last_buy_date, openid, tel, invited_times, reviewed_times
+#创建一个商品介绍，缩写为Cnnnnnnn-M20002370，如C0110050
+#tag review tag
+#系统自动创建一个静态页面，将购买过的用户信息都放进去
+#共同见证和支持的力量：有人，有商品，有用户，有农夫语，有评论，点击可前往购买
+def generate_review_invitation pospal_id, magento_id, post_id, buy_link
+end
+
 openids =  [   
-    'owHN1t0MhAd0Hv7KVwEhz65fBN-8',
-    'owHN1txf-Kpa8uwxAql7aLY67FAY',
-    'owHN1t60DTAxWPmo5LHlMEFQ1d5o',
-    'owHN1t99CmqMZ3MVk11xdF2z_tG0',
-    'owHN1t3GUvWYmKsxbwZdSpQDo4O4',
-    'owHN1t_RLY8FAHQpOa3GdXVoI6DM',
+    #'owHN1t0MhAd0Hv7KVwEhz65fBN-8', 'owHN1txf-Kpa8uwxAql7aLY67FAY', 'owHN1t60DTAxWPmo5LHlMEFQ1d5o', 'owHN1t99CmqMZ3MVk11xdF2z_tG0', 'owHN1t3GUvWYmKsxbwZdSpQDo4O4', 'owHN1t_RLY8FAHQpOa3GdXVoI6DM',
     'owHN1t0ETyOD1p_J324Gcb9twHuk'
 ]
 
-rds = Mysql2::Client.new(:host => ENV['RDS_AGENT'], :username => "psi_root", :port => '1401', :password => ENV['PSI_PASSWORD'])
+send_points_notice openids
+pg_test
+ms_test
