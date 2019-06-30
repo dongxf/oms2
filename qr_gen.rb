8f96e21732f9211823c2ab65d6ab4cbab18399f0
Franklin Dong <dongxf@foodtrust.cn>
2019-06-30 17:45:52 +0800

    qr_gen.rb部分完善

--- qr_gen.rb
+++ qr_gen.rb
@@ -44,23 +44,28 @@ def create_qualified_reviewers_table
 
         sql = ' CREATE TABLE ogoods.qualified_reviewers  (
           `goods_id` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+          `product_entity_id` int(11) NULL DEFAULT 0,
           `openid` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
           `avatar` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
           `nick_name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
           `phone` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
           `last_buy_date` datetime(6) NULL DEFAULT NULL,
-          `order_id` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+          `order_no` varchar(16) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
           `buy_times` int(11) NULL DEFAULT 0,
           `points_awarded` decimal(11, 2) NULL DEFAULT 0,
-          `points_transfered` decimal(11, 2) NULL DEFAULT 0,
-          PRIMARY KEY (`openid`) USING BTREE
+          `points_transfered` decimal(11, 2) NULL DEFAULT 0
         ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Compact;'
         res = rds.query(sql)
 
 end
 
+def update_qualified_reviwers customerList
+
+end
+
 #generate an Array
-def gen_customer_list_by_product_entity_id productId
+#productId是在M系统中的Product_Entity_ID, goodsCode是对应在银豹系统中的商品编码
+def gen_customer_list_by_product_entity_id productId, goodsCode
 
     list = []
     rds = Mysql2::Client.new(:host => ENV['RDS_AGENT'], :username => "psi_root", :port => '1401', :password => ENV['PSI_PASSWORD'])
@@ -73,66 +78,55 @@ def gen_customer_list_by_product_entity_id productId
         group by customer_id order by max(order_id) desc limit 0,1000;"
     res = rds.query(sql)
     res.each do |r|
-            list += [ { customer_id: r['customer_id'], phone: r['phone'], order_no: r['order_no'], order_date: r['created_at'] } ]
+            list += [ { product_id: productId, goods_code: goodsCode, customer_id: r['customer_id'], phone: r['phone'], order_no: r['order_no'], order_date: r['created_at'] } ]
     end
     return list
 end
 
 #to generate an Hash with includ customer information hash
-def extend_customer_hash list
+def extend_customer_list list
+    extendedList = []
+    list.each do |customer|
+            customer_entity_id = customer[:customer_id]
+            sql = " select m.entity_id as customer_id, at.value as avatar, m.email as email, la.value as lastname, cg.customer_group_code as groupname
+                from foodtrust.customer_entity_varchar as at
+                left join foodtrust.customer_entity as m
+                on m.entity_id = at.entity_id
+                left join foodtrust.customer_group as cg
+                on m.group_id = cg.customer_group_id
+                left join foodtrust.customer_entity_varchar as la
+                on la.entity_id = at.entity_id and la.attribute_id = 5
+                where at.attribute_id= 318 and at.entity_id = #{customer_entity_id}"
 
-    extendedList = {}
+            rds = Mysql2::Client.new(:host => ENV['RDS_AGENT'], :username => "psi_root", :port => '1401', :password => ENV['PSI_PASSWORD'])
+            res = rds.query(sql)
+            existed = customer
+            res.each do |r|
+                customer_id = r['customer_id']
+                openid = r['email'].gsub('@weixin.com','')
+                avatar = r['avatar']
 
-    #to generate an string such as '(1556, 550)'
-    inGroup = ' ('
-    ch.each { |key, value| inGroup += "#{key}," }
-    inGroup += ')'
-    inGroup.gsub!(',)',')')
-    #p inGroup
-
-    sql = ' select m.entity_id as customer_id, at.value as avatar, m.email as email, la.value as lastname, cg.customer_group_code as groupname
-        from foodtrust.customer_entity_varchar as at
-        left join foodtrust.customer_entity as m
-        on m.entity_id = at.entity_id
-        left join foodtrust.customer_group as cg
-        on m.group_id = cg.customer_group_id
-        left join foodtrust.customer_entity_varchar as la
-        on la.entity_id = at.entity_id and la.attribute_id = 5
-        where at.attribute_id= 318 and at.entity_id in ' + inGroup
-
-    rds = Mysql2::Client.new(:host => ENV['RDS_AGENT'], :username => "psi_root", :port => '1401', :password => ENV['PSI_PASSWORD'])
-    res = rds.query(sql)
-    res.each do |r|
-        customer_id = r['entity_id']
-        openid = r['email'].gsub('@weixin.com','')
-        avatar = r['avatar']
-
-        existed_info = list[customer_id]
-        #extneded_info = existed_info.store(openid: openid
+                existed.store(:openid, openid)
+                existed.store(:avatar, avatar)
+            end
+            extendedList += [ existed ]
     end
-
+    ap extendedList
     return extendedList
 end
 
-
 #初始化数据表，仅运行一次
-#create_qualified_reviewers_table #ONLY use for initialize table
+create_qualified_reviewers_table #ONLY use for initialize table
 
 #获得对应Magento某产品的用户信息表,可能有重复数据
 customerList = []
-customerList += gen_customer_list_by_product_entity_id(4312)
-customerList += gen_customer_list_by_product_entity_id(4313)
-customerList += gen_customer_list_by_product_entity_id(4314)
-#ap customerList
+customerList += gen_customer_list_by_product_entity_id(4312,'0120036')
+customerList += gen_customer_list_by_product_entity_id(4313,'0120036')
+customerList += gen_customer_list_by_product_entity_id(4314,'0120036')
+#customerList.each { |customer| puts "#{customer[:customer_id]} #{customer[:order_no]}" }
 
-#去掉重复的phone并将其转为用customer_id索引的Hash
-customerHash = {}
-customerList.each { |c| customerHash.store(c[:phone],c) }
-p customerHash.size
-#customerHash.each do | key, value| 
-#   p value[:phone]
-#end
+#为已有的客户列表扩张属性，例如openid, avatar
+extendedCustomerList = extend_customer_list customerList
+ap extendedCustomerList
 
-#为已有的Hash扩张属性，例如openid, avatar
-#extendedCustomerHash = extend_customer_hash customerHash
-#ap extendedCusterHash
+update_qualified_reviwers extendedCustomerList
