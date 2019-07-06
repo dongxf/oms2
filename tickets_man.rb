eaf1133a7b66e50032ea911913db345b68de4e1e
Franklin Dong <dongxf@foodtrust.cn>
2019-07-07 01:25:50 +0800

    let ticket_man.rb can read files under certain directory

--- tickets_man.rb
+++ tickets_man.rb
@@ -1,21 +1,41 @@
 #encoding: utf-8
-#
+
 #This file is used to pre-processed tickets read under access time limited
+
+require 'find'
 require 'awesome_print'
-load 'get_orders.rb'
+load    'get_orders.rb'
 
-stime = '2019-07-05 00:00:00'
-etime = '2019-07-05 23:59:59'
-#tickets = get_pospal_tickets_within stime, etime
-#IO.readlines(".\\list.txt").each { |line|  puts line[0..9] }
-#tickets=JSON.parse IO.readlines(".\\auto_import\\tickets\\tickets.json")[0]
-
-day = Date.today # day = Date.parse('2019-02-26')
-day_count = 48
-day_count.times do
-    stime = day.strftime('%Y-%m-%d') + " 00:00:00"
-    etime = day.strftime('%Y-%m-%d') + " 23:59:59"
-    puts "retrieving tickets during #{stime} - #{etime}"
-    tickets = get_pospal_tickets_within stime, etime #this function already save json file
-    day = day.prev_day
+=begin
+    Usage: to import all json data under certain direcoties
+    eg:    retrieve_json_data_since Date.today 1
+    eg:    retrieve_json_data_since Date.parse('2019-03-01'), 31
+=end
+def import_json_data
+    total = 0 
+    Find.find('.//auto_import//tickets') do |fn|
+        if fn.include? 'json'
+            tickets = JSON.parse IO.readlines(fn)[0]
+            total += tickets.size
+            puts "#{tickets.size} tickets in #{fn}"
+        end
+    end
+    puts "total: #{total}"
 end
+
+=begin
+    Usage: to retrieve tickets since
+    eg:    retrieve_json_data_since Date.today 1
+    eg:    retrieve_json_data_since Date.parse('2019-03-01'), 31
+=end
+def retrieve_json_data_since day, count
+    count.times do
+        stime = day.strftime('%Y-%m-%d') + " 00:00:00"
+        etime = day.strftime('%Y-%m-%d') + " 23:59:59"
+        puts "retrieving tickets during #{stime} - #{etime}"
+        #tickets = get_pospal_tickets_within stime, etime #this function already save json file
+        day = day.next_day
+    end
+end
+
+retrieve_json_data_since Date.parse('2019-03-01'), 1
