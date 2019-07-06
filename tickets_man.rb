4eb0cb8a302056ec606f59407549e67ade0c8e42
Franklin Dong <dongxf@foodtrust.cn>
2019-07-07 00:07:31 +0800

    improved access_remained in pospal_api, improved day initialized method in tickets_man.rb

--- tickets_man.rb
+++ tickets_man.rb
@@ -10,13 +10,12 @@ etime = '2019-07-05 23:59:59'
 #IO.readlines(".\\list.txt").each { |line|  puts line[0..9] }
 #tickets=JSON.parse IO.readlines(".\\auto_import\\tickets\\tickets.json")[0]
 
-day = Date.today
-130.times do
+day = Date.today # day = Date.parse('2019-02-26')
+day_count = 48
+day_count.times do
     stime = day.strftime('%Y-%m-%d') + " 00:00:00"
     etime = day.strftime('%Y-%m-%d') + " 23:59:59"
-    break_day = Date.parse('2019-06-06')
-    day = day.prev_day
-    next if day >= break_day
     puts "retrieving tickets during #{stime} - #{etime}"
     tickets = get_pospal_tickets_within stime, etime #this function already save json file
+    day = day.prev_day
 end
