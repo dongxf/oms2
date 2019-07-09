a351cef5d12d22c8336d24368d12bb1b17e8ba82
Franklin Dong <dongxf@foodtrust.cn>
2019-07-09 17:39:03 +0800

    improved start_time for current day in order_man.rb to save api calls quota

--- order_man.rb
+++ order_man.rb
@@ -8,7 +8,7 @@ load 'get_orders.rb'
 #puts 'eg: ruby order_man.rb 2019-07-09 2'
 
 the_day = Date.today
-counter = 2
+counter = 1
 silence_mode = false
 
 the_day = Date.parse(ARGV[0]) if ARGV[0]
@@ -17,8 +17,14 @@ silence_mode = true if ARGV[2] == '-s'
 
 jorders = []
 counter.times do 
-    stime = the_day.strftime('%Y-%m-%d') + " 00:00:00"
-    etime = the_day.strftime('%Y-%m-%d') + " 23:59:59"
+    if the_day == Date.today #没办法，就得节省API调用次数啊
+        stime = (DateTime.now - 1).to_time.strftime('%Y-%m-%d %H:%M:%S')
+        etime = (Time.now - 2).strftime('%Y-%m-%d %H:%M:%S')
+    else
+        stime = the_day.strftime('%Y-%m-%d') + " 00:00:00"
+        etime = the_day.strftime('%Y-%m-%d') + " 23:59:59"
+    end
+    puts "#{stime} - #{etime}"
     jorders += get_orders_within stime, etime
     the_day = the_day.prev_day
 end
