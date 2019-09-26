f165db0527309cd2d53a6f840f47d7ef93e95e9f
Franklin Dong <dongxf@foodtrust.cn>
2019-09-27 07:17:37 +0800

    update brief style

--- brief.rb
+++ brief.rb
@@ -59,8 +59,8 @@ oorders.each do |oorder|
     attention = "!!!!!!!" if oorder[:order_times] == 1
 
     info =  " #{oorder[:addr]} [#{oorder[:short_number]}]LFCR  #{oorder[:name]} #{oorder[:tel]} #{comment} #{attention}LFCR"
+    info += "  #{oorder[:tips]}\n" if oorder[:tips]!= ''
     info += "  :::#{oorder[:date_time]} #{oorder[:number]} #{sprintf("%.2f",oorder[:amount])}\n" # " :::" 用于生成派线表时作为分割识别
-    info += "  -- #{oorder[:tips]}\n" if oorder[:tips]!= ''
     routes_sum[line] += oorder[:amount]
 
     if line != '[X]'
