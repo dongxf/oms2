f04bb5852e5be81b4a137fbb631f20f89fd28b27
Franklin Dong <dongxf@foodtrust.cn>
2020-05-06 07:19:33 +0800

    重构export-user, 同一个地方生产SQL语句分别用于直接执行或导出语句

--- export-users.rb
+++ export-users.rb
@@ -8,13 +8,23 @@ require 'awesome_print'
 require 'spreadsheet' #用于写xls文件
 require 'simple-spreadsheet' #用于读取xlsx文件，spreadsheet gem读xlsx会报签名错误
 
+load 'rds_api.rb' #commitTrans queryRds
+
+=begin
+Usage:  export-users.rb [-w]
+        用于生成迁移用户数据所需的SQL语句
+        -w 将直接修改数据库
+        没有-w 将只生成SQL语句
+=end
+
+WRITE_MODE = ARGV[0]=='-w' ? true : false
+
 @rds = Mysql2::Client.new(:host => ENV['RDS_AGENT'], :username => "psi_root", :port => '1401', :password => ENV['PSI_PASSWORD'])
 
 def read_pospal_users
 
   recs = []
-
-  inq = "select * from ogoods.pospal_users where ignored = 0"
+  inq = "select * from ogoods.pospal_users where ignored = 0" #and crmeb_uid is NULL"  #when go production add this condition!!!
   res = @rds.query(inq)
 
   res.each do | r |
@@ -22,7 +32,6 @@ def read_pospal_users
     #basic fields
     fields = ['uid', 'number', 'name', 'phone', 'openid', 'unionid', 'avatar', 'discount', 'raw_data', 'points', 'discount', 'balance', 'address', 'created']
     fields.each { |f| rec[f]=r[f] }
-    #ap rec if rec['phone'].include? '136000600'  #just check
     recs += [rec]
   end
 
@@ -36,7 +45,7 @@ def save_to_excel names, items, fn
   book = Spreadsheet::Workbook.new
   sheet = book.create_worksheet :name => "sheet"
 
-#生成表头
+  #生成表头
   colIndex=0 #col index
   names.each do |name|
     sheet.row(0)[colIndex]=name
@@ -57,124 +66,116 @@ def save_to_excel names, items, fn
   puts "done. #{items.size}"
 end
 
-def gen_user_sql user, idx
+def createSingleUserSqls user, idx
 
-  sql = ''
-  rtime=Time.now.strftime("%H%M%S")
+  sqls = []
+
   grade = 101 - user['discount']
   levels = { 1 => 1, 2 => 2, 3 => 3, 4 => 4, 5 => 5, 6 => 6, 9 => 7, 16 => 8}
   level = levels[grade]
   if level.nil?
-    puts "error: #{idx} #{user['discount']} #{user['name']}"
-    return
+    puts ">>>ERROR: #{idx} #{user['discount']} #{user['name']}"
+    exit
   end
 
   memberships = { 1 => '百花蜜', 2 => '椴树蜜' , 3 => '藿香蜜', 4 => '党参蜜', 5 => '藏岩蜜', 6 => '生活家', 9 => '小伙伴', 16 => '在职工' }
   member = memberships [ grade ]
 
-  #注意sql中的field需要保持在同一列中
-  sql += "
-INSERT INTO `eb_user` (uid, account, pwd, real_name, birthday, card_id, mark, partner_id, group_id, nickname, avatar, phone, add_time, add_ip, last_time, last_ip, now_money, brokerage_price, integral, sign_num, status, level, spread_uid, spread_time, user_type, is_promoter, pay_count, spread_count, clean_time, addres, adminid, login_type, pospal_number ) VALUES (
+  #注意SQL中的fields需要保持在同一列中
+  sqls += ["INSERT INTO crmeb.eb_user (uid, account, pwd, real_name, birthday, card_id, mark, partner_id, group_id, nickname, avatar, phone, add_time, add_ip, last_time, last_ip, now_money, brokerage_price, integral, sign_num, status, level, spread_uid, spread_time, user_type, is_promoter, pay_count, spread_count, clean_time, addres, adminid, login_type, pospal_number ) VALUES (
   #{idx}, '#{user['phone']}', '9cf9542772e8a5904acd419e51d2011d', '#{@rds.escape user['name']}', 0, '', '', 
   0, 0, '#{@rds.escape user['name']}', '#{user['avatar']}', '#{user['phone']}', 1588339658, '128.0.0.1', 1588339658, '128.0.0.1', 
   #{sprintf('%2f',user['balance'])}, 0.00, #{sprintf('%2f',user['points'])}, 0, 1, #{level}, 
   0, 0, 'wechat', 0, 0, 0, 0, '#{@rds.escape user['address']}', 0, '', '#{user['number']}'
-);"
+  );"]
 
-  sql += "
-INSERT INTO `eb_wechat_user` VALUES (
+  sqls += ["INSERT INTO crmeb.eb_wechat_user VALUES (
   #{idx}, '#{user['unionid']}', '#{user['openid']}', NULL, '#{@rds.escape user['name']}', '#{user['avatar']}', 0, '广州', 'zh_CN', '广东', '中国', NULL, 0, NULL, 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, 'wechat'
-);"
+  );"]
 
-  sql +=  "
-INSERT INTO `eb_user_level` VALUES (#{idx}, #{idx}, #{level}, #{grade}, 1588552526, 1, 0, 1, '#用户#{user['name']}在#{rtime}由系统赠送会员等级成为#{member}会员', 0, 0, 1588552526, #{user['discount']});"
+  sqls +=  ["INSERT INTO crmeb.eb_user_level VALUES (#{idx}, #{idx}, #{level}, #{grade}, 1588552526, 1, 0, 1, '#用户#{user['name']}在#{Time.now.strftime('%Y年%m月%d日')}由系统赠送会员等级成为#{member}会员', 0, 0, 1588552526, #{user['discount']}
+  );"]
 
-  sql += "
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (1, #{idx}, 1, 1588552526);
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (2, #{idx}, 1, 1588552526);
-  " if level > 1
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (1, #{idx}, 1, 1588552526);"] if level > 0
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (2, #{idx}, 1, 1588552526);"] if level > 0
 
-  sql += "
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (3, #{idx}, 1, 1588552526);
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (4, #{idx}, 1, 1588552526);
-  " if level > 1
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (3, #{idx}, 1, 1588552526);"] if level > 1
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (4, #{idx}, 1, 1588552526);"] if level > 1
 
-  sql += "
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (5, #{idx}, 1, 1588552526);
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (6, #{idx}, 1, 1588552526);
-  " if level > 2
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (5, #{idx}, 1, 1588552526);"] if level > 2
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (6, #{idx}, 1, 1588552526);"] if level > 2
 
-  sql += "
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (7, #{idx}, 1, 1588552526);
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (8, #{idx}, 1, 1588552526);
-  " if level > 3
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (7, #{idx}, 1, 1588552526);"] if level > 3
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (8, #{idx}, 1, 1588552526);"] if level > 3
 
-  sql += "
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (9, #{idx}, 1, 1588552526);
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (10, #{idx}, 1, 1588552526);
-  " if level > 4
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (9, #{idx}, 1, 1588552526);"] if level > 4
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (10, #{idx}, 1, 1588552526);"] if level > 4
 
-  sql += "
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (11, #{idx}, 1, 1588552526);
-INSERT INTO `eb_user_task_finish` (task_id, uid, status, add_time) VALUES (12, #{idx}, 1, 1588552526);
-  " if level > 5
-  
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (11, #{idx}, 1, 1588552526);"] if level > 5
+  sqls += ["INSERT INTO crmeb.eb_user_task_finish (task_id, uid, status, add_time) VALUES (12, #{idx}, 1, 1588552526);"] if level > 5
+
   # 这里的link_id就这么填‘1’，不知状况
-  sql += "
-INSERT INTO `eb_user_bill` (uid, link_id, pm, title, category, type, number, balance, mark, add_time, status, take ) VALUES (
+  sqls += ["INSERT INTO crmeb.eb_user_bill (uid, link_id, pm, title, category, type, number, balance, mark, add_time, status, take ) VALUES (
   #{idx}, '1', 1, 'FC4.0系统初始导入', 'now_money', 'system_add', #{sprintf('%2f', user['balance'])}, #{sprintf('%2f', user['balance'])}, 
-  'FC3.0系统余额平移', 1588583622, 1, 0);" if user['balance'] >= 0.01
+  'FC3.0系统余额平移', 1588583622, 1, 0);"] if user['balance'] >= 0.01
 
-  sql += "
-INSERT INTO `eb_user_bill` (uid, link_id, pm, title, category, type, number, balance, mark, add_time, status, take ) VALUES (
+  sqls += ["INSERT INTO crmeb.eb_user_bill (uid, link_id, pm, title, category, type, number, balance, mark, add_time, status, take ) VALUES (
   #{idx}, '1', 1, 'FC4.0系统初始导入', 'integral', 'system_add', #{sprintf('%2f',user['points'])}, #{sprintf('%2f', user['points'])}, 
-  'FC3.0系统积分平移', 1588583622, 1, 0);" if user['points'] >= 0.01
+  'FC3.0系统积分平移', 1588583622, 1, 0);"] if user['points'] >= 0.01
 
-
-  print('.')
-  sql += "\n"
-  return sql
+  return sqls
 
 end
 
-def gen_sql_cmd users
+def clearCrmebUsersSqls
+  return [ 
+    "delete from crmeb.eb_user where 1=1;", 
+    "delete from crmeb.eb_wechat_user where 1=1;",
+    "delete from crmeb.eb_user_level where 1=1;",
+    "delete from crmeb.eb_user_task_finish where 1=1;",
+    "delete from crmeb.eb_user_bill where 1=1;" 
+  ]
+end
 
-  sql = "
-delete from crmeb.eb_user where 1=1;
-delete from crmeb.eb_wechat_user where 1=1;
-delete from crmeb.eb_user_level where 1=1;
-delete from crmeb.eb_user_task_finish where 1=1;
-delete from crmeb.eb_user_bill where 1=1;
-BEGIN;
-  "
+def createCrmebUsers users
+
+  sqls = []
+
+  inqs = clearCrmebUsersSqls
+  commitTrans inqs if WRITE_MODE
+  sqls += inqs
 
   idx = 1
   users.each do |user|
-
     if user['openid'] != ''
-      sql += gen_user_sql(user, idx)
+
+      inqs = createSingleUserSqls user, idx
+      commitTrans inqs if WRITE_MODE
+      sqls += inqs
+
+      inq = "update ogoods.pospal_users set crmeb_uid = #{idx} where uid = #{user['uid']};"
+      queryRds(inq) if WRITE_MODE
+      sqls += [inq]
+
       idx += 1
+
     end
-
   end
-  sql += "COMMIT;"
 
-  fn = "import-pospal-users.sql"
-  File.open(fn,"w:UTF-8") { |f| f.write sql }
-  
-  puts ("done #{users.size}")
-  return sql
-  
+  puts "done. #{sqls.size}"
+  return sqls
+
 end
 
 puts "reading all recs from pospal..."
 users = read_pospal_users
+
+puts "save_to_excel..."
 fields = ['uid', 'number', 'name', 'phone', 'openid', 'unionid', 'avatar', 'discount', 'raw_data', 'points', 'discount', 'balance', 'address', 'created']
+save_to_excel fields, users, "pospal-users-all.xls"
 
-=begin
-#puts "writing excel file..."
-save_to_excel fields, users, './export/pospal-users-all.xls'
-=end
+puts "creating crmeb users..."
+sqls = createCrmebUsers users
 
-puts "generating sql file..."
-sql = gen_sql_cmd users
+fn = "import-pospal-users.sql"
+File.open(fn,"w:UTF-8") { |f| f.write sqls.join("\n") }
