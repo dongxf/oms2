d77b0939bf892db0f779841b6057ed4d78705067
Franklin Dong <dongxf@foodtrust.cn>
2020-05-06 07:19:33 +0800

    重构export-user, 同一个地方生产SQL语句分别用于直接执行或导出语句

---
+++ rds_api.rb
@@ -0,0 +1,24 @@
+@rds = Mysql2::Client.new(:host => ENV['RDS_AGENT'], :username => "psi_root", :port => '1401', :password => ENV['PSI_PASSWORD'])
+
+def commitTrans inqs
+  @rds.query('begin')
+    inqs.each do |inq|
+      begin
+        @rds.query(inq)
+      rescue => e
+        @rds.query('rollback')
+        puts ">>>ERROR: #{e}"
+        exit
+      end
+    end
+  @rds.query('commit')
+end
+
+def queryRds inq
+  begin
+    @rds.query(inq)
+  rescue => e
+    puts ">>>ERROR: #{e}"
+    exit
+  end
+end
